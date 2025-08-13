004cf35b67156979a53de6859181fe28668988c3
Vivid-designs <lario@vividgraphics.co.za>
2025-08-13 22:50:07 +0200

    installed bootstrap

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { useEffect } from 'react'
+
+export default function MyApp({ Component, pageProps }) {
+	useEffect(() => {
+		import('bootstrap/dist/js/bootstrap.bundle.min.js')
+	}, [])
+	return <Component {...pageProps} />
+}
+
+
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 export default function Home() {
     return (
-        <h1>Hello World</h1>
+		<div className="container py-5">
+			<h1 className="display-4 mb-4">Hello World</h1>
+			<button className="btn btn-primary">Bootstrap Button</button>
+		</div>
     )
 }
