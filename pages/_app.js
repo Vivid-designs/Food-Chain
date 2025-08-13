74681477c8132903e74fe42351d2e4327d400a58
Vivid-designs <lario@vividgraphics.co.za>
2025-08-13 23:14:17 +0200

    updated home page with basic components

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '../src/styles/homepage.css'
 import { useEffect } from 'react'
 
 export default function MyApp({ Component, pageProps }) {
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
+"use client";
+
 export default function Home() {
     return (
-		<div className="container py-5">
-			<h1 className="display-4 mb-4">Hello World</h1>
-			<button className="btn btn-primary">Bootstrap Button</button>
+		<div className="centered-container">
+			<h1 className="welcome-title">Hello and Welcome to Food Chain</h1>
+			<p className="welcome-description">
+				Because life is to short to be arguing over bills and food.
+			</p>
+			<button className="btn btn-primary">Take A Picture</button>
+            <p>Take a picture of the bill to get started</p>
 		</div>
     )
 }
